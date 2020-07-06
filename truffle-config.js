496570a3248774d8c1c3c98779e83a8ceecffbee
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-07-06 19:12:48 +0100

    update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,12 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+ // const HDWalletProvider = require('@truffle/hdwallet-provider');
+ // const infuraKey = '';
+ // const infuraURL = 'https://kovan.infura.io/v3/';
+
+ // const fs = require('fs');
+ // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -42,11 +43,17 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+     development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+     },
+
+     kovan: {
+      provider: () => new HDWalletProvider(mnemonic, infuraURL),
+      network_id: 42, // Kovan's network id
+      gas: 9000000,
+     },
 
     // Another network with more advanced options...
     // advanced: {
@@ -85,7 +92,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+       version: "0.6.10",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
