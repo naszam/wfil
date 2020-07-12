751c64f161e487a46891e4460018fec7fffb7517
Cristiam Da Silva <cristiam86@gmail.com>
2020-07-12 17:34:51 +0200

    added custom theme

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,30 @@
 import React from "react";
 import { DrizzleContext } from "@drizzle/react-plugin";
 import { Drizzle } from "@drizzle/store";
+import {ThemeProvider} from 'styled-components'
+import {theme} from 'rimble-ui'
+
 import drizzleOptions from "./drizzleOptions";
 import Routes from './Routes';
+import customTheme from './theme';
 
 const drizzle = new Drizzle(drizzleOptions);
+const appTheme = { ...theme, ...customTheme };
 
 const App = () => {
   return (
-    <DrizzleContext.Provider drizzle={drizzle}>
-      <DrizzleContext.Consumer>
-        {drizzleContext => {
-          const { initialized } = drizzleContext;
+    <ThemeProvider theme={appTheme}>
+      <DrizzleContext.Provider drizzle={drizzle}>
+        <DrizzleContext.Consumer>
+          {drizzleContext => {
+            const { initialized } = drizzleContext;
 
-          if (!initialized) return "Loading..."
-          return <Routes />
-        }}
-      </DrizzleContext.Consumer>
-    </DrizzleContext.Provider>
+            if (!initialized) return "Loading..."
+            return <Routes />
+          }}
+        </DrizzleContext.Consumer>
+      </DrizzleContext.Provider>
+    </ThemeProvider>
   );
 }
 
---
+++ app/src/theme.js
@@ -0,0 +1,7 @@
+export default {
+  colors: {
+    primary: '#0090ff',
+    'primary-light': '#69c0ff',
+    'primary-dark': '#0063cb',
+  }
+}
