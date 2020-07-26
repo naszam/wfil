d499e5f0d20dc9dab13b469bfee9715b7973e2dd
Cristiam Da Silva <cristiam86@gmail.com>
2020-07-26 11:50:38 +0200

    added logo

--- app/src/components/headers/MainHeader/MainHeader.js
+++ app/src/components/headers/MainHeader/MainHeader.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import { Flex, Box, MetaMaskButton } from 'rimble-ui';
 
+import { ReactComponent as Logo } from '../../Icon/logo.svg';
+
 const HeaderBg = styled.div`
   background-color: ${props => props.theme.colors.primary};
 `;
@@ -11,7 +13,7 @@ const MainHeader = () => {
     <HeaderBg>
       <Flex>
         <Box p={3} width={1 / 2}>
-          Logo
+          <Logo style={{ width: '45px', height: '45px' }} />
         </Box>
         <Box p={3} width={1 / 2} textAlign="right">
           <MetaMaskButton.Outline>Connect with MetaMask</MetaMaskButton.Outline>
