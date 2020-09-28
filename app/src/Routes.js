420c7abfd8998a2c9f933e8175f9ac7aecdda7d9
Cristiam Da Silva <cristiam86@gmail.com>
2020-09-28 12:20:36 +0200

    updated router to hashrouter and fix fleek missing redirect

--- app/src/Routes.js
+++ app/src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Wallet from './pages/Wallet';
