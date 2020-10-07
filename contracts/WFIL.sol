6b82b07a6c2075ed1f635b45a40e511d28257ddc
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-10-07 18:01:06 +0100

    add wrap/unwrap fee

--- contracts/WFIL.sol
+++ contracts/WFIL.sol
@@ -22,21 +22,32 @@ import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
 
 contract WFIL is Ownable, AccessControl, ERC20, ERC20Pausable {
 
+    /// @dev Libraries
+    using SafeMath for uint256;
+
     /// @dev Roles
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant FEE_SETTER_ROLE = keccak256("FEE_SETTER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
-    /// @dev Events
-    event Wrapped(address to, uint amount);
-    event Unwrapped(string filaddress, uint amount);
+    /// @dev Data
+    uint8 private _fee;
+    address private _feeTo;
 
-    constructor()
+    /// @dev Events
+    event Wrapped(address to, uint wrapOut, uint wrapFee);
+    event Unwrapped(string filaddress, uint unwrapOut, uint unwrapFee);
+
+    constructor(address feeTo_)
         ERC20("Wrapped Filecoin", "WFIL")
     {
         _setupRole(DEFAULT_ADMIN_ROLE, owner());
 
         _setupRole(MINTER_ROLE, owner());
         _setupRole(PAUSER_ROLE, owner());
+        _setupRole(FEE_SETTER_ROLE, owner());
+
+        _setFeeTo(feeTo_);
     }
 
     /// @notice Fallback function
@@ -56,14 +67,32 @@ contract WFIL is Ownable, AccessControl, ERC20, ERC20Pausable {
         _;
     }
 
+    modifier onlyFeeSetter() {
+        require(hasRole(FEE_SETTER_ROLE, msg.sender), "Caller is not the fee setter");
+        _;
+    }
+
+    function setFee(uint8 fee) external onlyFeeSetter returns (bool) {
+        _fee = fee;
+        return true;
+    }
+
+    function setFeeTo(address feeTo) external onlyFeeSetter returns (bool) {
+        _setFeeTo(feeTo);
+        return true;
+    }
+
     /// @notice Mint new WFIL
     /// @dev Access restricted only for Minters
     /// @param to Address of the recipient
     /// @param amount Amount of WFIL issued
     /// @return True if WFIL is successfully wrapped
     function wrap(address to, uint amount) external onlyMinter returns (bool) {
-        _mint(to, amount);
-        emit Wrapped(to, amount);
+        uint wrapFee = amount.mul(_fee).div(1000);
+        uint wrapOut = amount.sub(wrapFee);
+        _mint(_feeTo, wrapFee);
+        _mint(to, wrapOut);
+        emit Wrapped(to, wrapOut, wrapFee);
         return true;
     }
 
@@ -73,8 +102,11 @@ contract WFIL is Ownable, AccessControl, ERC20, ERC20Pausable {
     /// @param amount The amount of WFIL to unwrap
     /// @return True if WFIL is successfully unwrapped
     function unwrap(string calldata filaddress, uint amount) external returns (bool) {
-        _burn(msg.sender, amount);
-        emit Unwrapped(filaddress, amount);
+        uint unwrapFee = amount.mul(_fee).div(1000);
+        uint unwrapOut = amount.sub(unwrapFee);
+        _transfer(msg.sender, _feeTo, unwrapFee);
+        _burn(msg.sender, unwrapOut);
+        emit Unwrapped(filaddress, unwrapOut, unwrapFee);
         return true;
     }
 
@@ -110,6 +142,12 @@ contract WFIL is Ownable, AccessControl, ERC20, ERC20Pausable {
     function unpause() external {
         require(hasRole(PAUSER_ROLE, msg.sender), "WFIL: must have pauser role to unpause");
         _unpause();
+    }
+
+    function _setFeeTo(address feeTo) private {
+      require(_feeTo != address(0), "WFIL: set to zero address");
+      require(_feeTo != address(this), "WFIL: set to contract address");
+      _feeTo = feeTo;
     }
 
     /// @notice Prevent transfer to the token contract
