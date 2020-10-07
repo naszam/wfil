3c6f1d6ec6e53cc31b64a458d414584ee9e30b0d
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-10-07 18:30:40 +0100

    update migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 var WFIL = artifacts.require("WFIL");
+const kovan = require('./kovan');
 
 module.exports = function(deployer) {
 
-	deployer.deploy(WFIL);
+	deployer.deploy(WFIL, kovan.wfil.feeTo);
 };
---
+++ migrations/kovan.js
@@ -0,0 +1,12 @@
+// This file contains Kovan Addresses
+
+module.exports = {
+wfil: {
+  feeTo: '0xF5f7393dbE345f566b5A6580c2455Bdcdd134A95',
+},
+
+opengsn: {
+   trustedForwarder: '',
+}
+
+};
