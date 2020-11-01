bca92e6246d65466214cf90ec2b784895779a10a
Cristiam Da Silva <cristiam86@gmail.com>
2020-11-01 14:40:13 +0100

    wallet persintence service

---
+++ app/src/components/Wallet/db.js
@@ -0,0 +1,10 @@
+const LOCAL_STORAGE_KEY = 'wfil_wallet_v0.1';
+
+export function getWallet() {
+  const lsData = window.localStorage.getItem(LOCAL_STORAGE_KEY);
+  return lsData ? JSON.parse(lsData) : null;
+}
+
+export function saveWallet(wallet) {
+  window.localStorage.setItem(LOCAL_STORAGE_KEY, wallet);
+}
