73014f52d92aa1974fb01f0833008ec13a1c4dd9
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-11-03 16:35:25 +0000

    update migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,5 +3,5 @@ const rinkeby = require('./rinkeby');
 
 module.exports = function(deployer) {
 
-	deployer.deploy(WFIL, rinkeby.wfil.feeTo, rinkeby.wfil.fee);
+	deployer.deploy(WFIL, rinkeby.wfil.minter, rinkeby.wfil.feeTo, rinkeby.wfil.fee);
 };
